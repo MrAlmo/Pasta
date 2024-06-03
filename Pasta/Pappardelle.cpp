31f57d7b65b2aeec50d72aa3a2501ad135a61236
MrAlmo <tutrest5003@gmail.com>
2024-06-03 19:44:38 +0200

    wrote discription of Pappardelle constructor in .cpp

--- Pasta/Pappardelle.cpp
+++ Pasta/Pappardelle.cpp
@@ -1,5 +1,9 @@
 #include "Pappardelle.h"
 
+Pappardelle::Pappardelle(double _width, double _length, double _cookingTime):
+	Pasta(_width, _length, _cookingTime)
+{}
+
 std::string Pappardelle::Type() {
 	return "Pappardelle";
 }
--- Pasta/Pappardelle.h
+++ Pasta/Pappardelle.h
@@ -3,9 +3,8 @@
 class Pappardelle : public Pasta {
 
 public:
-	Pappardelle(double _width, double _length, double _cookingTime):
-		Pasta(_width, _length, _cookingTime)
-	{}
+	Pappardelle(double _width, double _length, double _cookingTime);
+		
 
 	std::string Type();
 	std::string Discription();
