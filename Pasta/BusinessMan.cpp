98e50b91fbdd7bee6f984590cb418d2c5c49f6a8
MrAlmo <tutrest5003@gmail.com>
2024-06-18 22:14:11 +0200

    modify BusinessMan's methods

--- Pasta/BusinessMan.cpp
+++ Pasta/BusinessMan.cpp
@@ -5,9 +5,26 @@ BusinessMan::BusinessMan(std::string _name, std::string _surname, int _age):
 {}
 
 void BusinessMan::To_Eat(Pasta* a) {
-
+	To_Cook(a);
+	std::cout << "Sits down at the table and elegantly begins to eat." << std::endl;
+	for (int i = 3; i > 0; i--) {
+		std::cout << i << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	std::cout << "Done!";
 }
 
 void BusinessMan::To_Cook(Pasta* a) {
-
+	std::cout << "Take "<<a->Type(); 
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	if (a->Type() == "Lasagna") {
+		std::cout << " then opened the oven and put the " << a->Type() << " in there." << std::endl;
+	}
+	else
+		std::cout << " then put the " << a->Type() << " in a pot of boiling water." << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout << "Wait " << a->GetCookingTime() << " minutes."<<std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::cout << "Well Done!" << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
--- Pasta/BusinessMan.h
+++ Pasta/BusinessMan.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <thread>
 #include "Human.h"
 
 class BusinessMan : public Human {
