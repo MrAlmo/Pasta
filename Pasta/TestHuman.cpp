08f6a5ab79cfcd18ba1dff6f51d4a05325709515
MrAlmo <tutrest5003@gmail.com>
2024-06-18 23:09:54 +0200

    create tests for Human

---
+++ Pasta/TestHuman.cpp
@@ -0,0 +1,19 @@
+#include "TestHuman.h"
+
+void testDisplayAllParameters(Human* a) {
+	std::cout << a->GetName() << std::endl;
+	std::cout << a->GetSurname() << std::endl;
+	std::cout << a->GetAge() << std::endl;
+}
+
+void testTo_Eat(Human* a, Pasta* b) {
+	a->To_Eat(b);
+}
+
+void testTo_Cook(PoorMan a, Pasta* b) {
+	a.To_Cook(b);
+}
+void testToCook(BusinessMan a, Pasta* b) {
+	a.To_Cook(b);
+}
+
---
+++ Pasta/TestHuman.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Human.h"
+#include "PoorMan.h"
+#include "BusinessMan.h"
+
+void testDisplayAllParameters(Human* a);
+
+void testTo_Eat(Human* a, Pasta* b);
+
+void testTo_Cook(PoorMan a, Pasta* b);
+void testToCook(BusinessMan a, Pasta* b);
